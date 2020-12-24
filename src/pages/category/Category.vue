397aecc3f38dddea3a77252ec9936c7894c2b4eb
小武 <570518277@qq.com>
2020-12-24 15:19:11 +0800

    暂存

---
+++ src/pages/category/Category.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="category">
+    分类
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Category'
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/mine/Mine.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="mine">
+    个人中心
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mine'
+}
+</script>
+
+<style>
+</style>
